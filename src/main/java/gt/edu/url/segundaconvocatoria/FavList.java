876aae3d3452ee6f79efb03679df4d5bab98f619
Ana Lucía Godinez <anagod98@outlook.com>
2017-12-04 08:31:32 -0600

    Commit

---
+++ src/main/java/gt/edu/url/segundaconvocatoria/FavList.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.url.segundaconvocatoria;
+
+/**
+ *
+ * @author Ana Godínez
+ */
+public class FavList implements FavoriteList{
+    
+    private int Favorites,access;
+    
+    
+    public FavList(int Favorites, int access){
+        
+        super();
+        this.Favorites = Favorites;
+        this.access = access;
+    }
+        
+    public int getAcces(){
+        int e;
+        //return e;
+    }
+        
+      
+    
+    
+    
+    
+    
+    @Override
+    public void access(Object e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Object e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterable getFavorites(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
